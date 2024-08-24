7c4a73ea363e19565fe556a2e76798635f658c09
Rohan Nair <rn@rohan.ai>
2024-08-23 22:31:48 -0400

    add inane healthcheck

---
+++ src/routes/app/forms/api/v2/health/integrity/+server.ts
@@ -0,0 +1,10 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+  return new Response(JSON.stringify({ status: 'ok' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
